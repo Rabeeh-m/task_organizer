22d9f0d27024b0276b565cbdbdb5031bdb98795a
Muhammed Rabeeh <rabeehm1999@gmail.com>
2025-08-08 11:08:17 +0530

    chore: complete first stable version

--- task_organizer/settings.py
+++ task_organizer/settings.py
@@ -1,7 +1,6 @@
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-import dj_database_url
 
 load_dotenv()
 
@@ -73,22 +72,6 @@ DATABASES = {
     }
 }
 
-if os.environ.get('RENDER'):
-    # Render environment (use PostgreSQL)
-    DATABASES = {
-        'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
-            conn_max_age=600
-        )
-    }
-else:
-    # Local environment (use SQLite)
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
